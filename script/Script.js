5d4d6812d5833b6a5dd1b40e5614ebfba5da93d2
Ajdevise <ajdevise151@hotmail.com>
2021-01-26 09:08:48 +0100

    added range component

---
+++ script/Script.js
@@ -0,0 +1,44 @@
+const monthlyRadio = document.querySelector("#monthly");
+const yearlyRadio = document.querySelector("#yearly");
+const priceToggleComponent = document.querySelector(".slider");
+
+const addActiveClassToPriceToggleComponent = () => {
+  priceToggleComponent.classList.add("active");
+}
+
+const removeActiveClassFromPriceToggleComponent = () => {
+  priceToggleComponent.classList.remove("active");
+}
+
+const onMonthlyRadioChecked = () => {
+  removeActiveClassFromPriceToggleComponent();
+}
+
+const onYearlyRadioChecked = () => {
+  addActiveClassToPriceToggleComponent();
+}
+
+const togglePriceToggleComponentClasses = () => {
+  if(priceToggleComponent.classList.contains("active")){
+    removeActiveClassFromPriceToggleComponent();
+  }else {
+    addActiveClassToPriceToggleComponent();
+  }
+}
+
+const checkProperRadioButton = () => {
+  if(yearly.checked){
+    monthly.checked = true;
+  }else{
+    yearly.checked = true;
+  }
+}
+
+const onButtonClick = () => {
+  togglePriceToggleComponentClasses();
+  checkProperRadioButton();
+}
+
+priceToggleComponent.addEventListener("click", onButtonClick);
+monthly.addEventListener("click", onMonthlyRadioChecked);
+yearly.addEventListener("click", onYearlyRadioChecked);
